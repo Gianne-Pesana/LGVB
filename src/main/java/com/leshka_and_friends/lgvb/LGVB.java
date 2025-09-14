4b887cfbc155354bd565b23422108fd3749c424a
Gianne-Pesana <gpesana@addu.edu.ph>
2025-09-15 02:29:34 +0800

    panel

--- src/main/java/com/leshka_and_friends/lgvb/LGVB.java
+++ src/main/java/com/leshka_and_friends/lgvb/LGVB.java
@@ -1,16 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.leshka_and_friends.lgvb;
+
 import com.leshka_and_friends.lgvb.controller.AuthController;
 import com.leshka_and_friends.lgvb.view.MainView;
+import com.leshka_and_friends.lgvb.view.components.panels.CardPanel;
 import com.leshka_and_friends.lgvb.view.test.AuthUITest;
 import com.leshka_and_friends.lgvb.view.test.CardTest;
 import com.leshka_and_friends.lgvb.view.themes.*;
 import com.leshka_and_friends.lgvb.view.ui_utils.ThemeGlobalDefaults;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
 /**
  *
@@ -29,16 +33,29 @@ public class LGVB {
 //        SwingUtilities.invokeLater(() -> {
 //            new ThemeDemo(UIManager.getLookAndFeel()).setVisible(true);
 //        });
-
         SwingUtilities.invokeLater(() -> {
-            AuthController authController = new AuthController();
-            authController.start();
+            cardTest();
+//            AuthController authController = new AuthController();
+//            authController.start();
 //            new CardTest();
 //            new MainView();
 //            AuthUITest test = new AuthUITest();
 //            test.showRegisterDialog();
 //            test.showLoginDialog();
-            
+
+        });
+    }
+
+    private static void cardTest() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setSize(500, 500);
+            frame.setLayout(new FlowLayout());
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            CardPanel cp = new CardPanel("Card1");
+            frame.add(cp);
+            frame.setVisible(true);
         });
     }
 }
--- src/main/java/com/leshka_and_friends/lgvb/view/components/panels/CardPanel.java
+++ src/main/java/com/leshka_and_friends/lgvb/view/components/panels/CardPanel.java
@@ -4,10 +4,14 @@
  */
 package com.leshka_and_friends.lgvb.view.components.panels;
 
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.leshka_and_friends.lgvb.view.ui_utils.SVGUtils;
+import com.leshka_and_friends.lgvb.view.ui_utils.ThemeGlobalDefaults;
 import com.leshka_and_friends.lgvb.view.ui_utils.ThemeManager;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -16,6 +20,12 @@ import javax.swing.border.EmptyBorder;
  */
 public class CardPanel extends RoundedPanel {
     private String cardName;
+    
+    private int baseWidth;
+    private int baseHeight;
+    
+    private JPanel headerPanel;
+    
 
     public CardPanel(String cardName) {
         this.cardName = cardName;
@@ -25,22 +35,51 @@ public class CardPanel extends RoundedPanel {
     }
     
     private void initComponents() {
+        baseWidth = ThemeGlobalDefaults.getScaledInt("card.width");
+        baseHeight = ThemeGlobalDefaults.getScaledInt("card.height");
+        Dimension cardSize = new Dimension(baseWidth, baseHeight);
+//        Dimension testDim = new Dimension(270, 170);
+        setPreferredSize(cardSize);
+        setMaximumSize(cardSize);
         
+        ThemeManager.putThemeAwareProperty(this, "background: $Card1.background;");
+        
+        setBorder(BorderFactory.createEmptyBorder(7,21,7,21));
+        
+        headerPanel = createHeaderPanel();
     }
-    
-    
-    
-    
-    
-    
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame();
-            frame.setSize(500, 500);
-            CardPanel cp = new CardPanel("Card1");
-            frame.add(cp);
-            frame.setVisible(true);
-        });
+
+    private JPanel createHeaderPanel() {
+        JPanel panel = new JPanel();
+        Dimension dim = new Dimension(
+                Integer.MAX_VALUE, 
+                ThemeGlobalDefaults.getScaledInt("card.header.height")
+        );
         
+        panel.setLayout(new BorderLayout());
+        ThemeManager.putThemeAwareProperty(panel, "background: $LGVB.primary");
+        
+        JPanel logoPanel = new JPanel();
+        ThemeManager.putThemeAwareProperty(logoPanel, "background: $LGVB.primary");
+        logoPanel.setPreferredSize(new Dimension(
+                ThemeGlobalDefaults.getScaledInt("card.logo.width"), 
+                ThemeGlobalDefaults.getScaledInt("card.logo.size")
+            )
+        );
+        
+        
+        
+        JLabel logoLabel = new JLabel();
+        FlatSVGIcon logoIcon = SVGUtils.loadIcon(
+                ThemeGlobalDefaults.getString("cards.logo.path"), 
+                ThemeGlobalDefaults.getScaledInt("card.logo.size")
+        );
+        logoIcon.setColorFilter(SVGUtils.createColorFilter("LGVB.foreground"));
+        
+        logoLabel.setIcon(logoIcon);
+        logoLabel.setPreferredSize(new Dimension());
+        
+        
+        return panel;
     }
 }
