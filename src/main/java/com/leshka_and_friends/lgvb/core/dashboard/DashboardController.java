b7e80e2cb704eba5d0dbd8c627ca38013988439a
Gianne-Pesana <gpesana@addu.edu.ph>
2025-11-03 00:51:03 +0800

    before transfer impl bck 2

--- src/main/java/com/leshka_and_friends/lgvb/core/dashboard/DashboardController.java
+++ src/main/java/com/leshka_and_friends/lgvb/core/dashboard/DashboardController.java
@@ -7,6 +7,7 @@ package com.leshka_and_friends.lgvb.core.dashboard;
 import com.leshka_and_friends.lgvb.view.testUI.DepositTestView;
 import com.leshka_and_friends.lgvb.core.app.AppFacade;
 import com.leshka_and_friends.lgvb.view.MainView;
+import com.leshka_and_friends.lgvb.view.testUI.TransferTestView;
 import com.leshka_and_friends.lgvb.view.ui_utils.OutputUtils;
 
 import java.awt.*;
@@ -43,6 +44,8 @@ public class DashboardController {
 
         mainView.getDashboardPanel().getMenuItemButton("Send").addActionListener(() -> {
             System.out.println("Send Btn Clicked");
+            TransferTestView ttv = new TransferTestView();
+            ttv.setVisible(true);
 
         });
 
